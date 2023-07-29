40308df9e1b8acda5d0f16fb83306af160862d89
pce0303 <ivychoen@gmail.com>
2023-07-29 17:21:25 +0900

    loginpage files

---
+++ ws/src/routes/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Login.css';
+
+function LoginPage() {
+    return (
+        <div className="login_container">
+            
+        </div>
+    )
+}
+
+export default LoginPage;
