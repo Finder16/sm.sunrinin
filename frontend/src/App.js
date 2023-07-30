37c7d17f782939df1c06e5b97962032b6bf97999
pce0303 <ivychoen@gmail.com>
2023-07-30 22:29:04 +0900

    Navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { HashRouter, Route } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './routes/home';
 import Navigation from './components/Navigation';
 import Login from './routes/login'
@@ -11,10 +11,12 @@ function App() {
   return (
     <HashRouter>
       <Navigation />
+      <Routes>
       <Route path="/" exact={true} component={Home} />
       <Route path="/login" component={Login} />
       <Route path="/signup" component={Signup} />
       <Route path="/profile" component={Profile} />
+      </Routes>
     </HashRouter>
   );
 }
