02b696fe490f6236f184f195136d379ecc2dfb5a
pce0303 <ivychoen@gmail.com>
2023-07-30 21:54:09 +0900

    file set

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { HashRouter, Route } from 'react-router-dom';
+import 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Route path="/" exact={true} component={} />
+      <Route path="/about" component={} />
+      <Route path="/movie-detail" component={} />
+    </HashRouter>
   );
 }
 
