3baa6f1f2c2cfda04bb7dc5a06c445b5563871a7
pce0303 <ivychoen@gmail.com>
2023-08-16 01:20:29 +0900

    yarn setting...

--- frontend/src/routes/login.js
+++ frontend/src/routes/login.js
@@ -3,13 +3,11 @@ import './login.css';
 
 function Login() {
     return (
-        <form method="post" action="" className="loginForm">
-            <div className="loginBox">
-                <h1 className="loginTitle">Log In</h1>
-                <input type="text" className="loginUsername" placeholder="Username"></input>
-                <input type="password" className="loginPassword" placeholder="Password"></input>
-                <button type="submit" className="loginButton">Login</button>
-            </div>
+        <form method="post" action="/login" className="loginForm">
+            <h1 className="loginTitle">Log In</h1>
+            <input type="text" className="loginUsername" placeholder="Username"></input>
+            <input type="password" className="loginPassword" placeholder="Password"></input>
+            <button type="submit" className="loginButton">Login</button>
         </form>
     )
 }
--- frontend/src/routes/profile.js
+++ frontend/src/routes/profile.js
@@ -1,13 +1,18 @@
 import React from "react";
+import './profile.css';
+import profileImage from '../profileImage.png';
+import observationImage from '../observationImage.jpeg';
+
+// const public/img/observationImage.jpeg
 
 function Profile() {
     return (
         <div className="profileBox">
-            <img src="" alt="profileImage" ></img>
+            <img src={profileImage} alt="profileImage" className="profileImage"></img>
             <p className="myName">이재영</p>
             <p className="myInfo">자기소개 : 이재영 입니다.</p>
             <p className="observationDate">관측 날짜 : 2023-06-14</p>
-            <img alt="observationImage"></img>
+            <img src={observationImage} alt="observationImage" className="observationImage"></img>   
             <textarea className="commentContent"></textarea>
             <button className="commentButton">코멘트 등록</button>
         </div>
