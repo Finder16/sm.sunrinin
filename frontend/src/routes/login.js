f5c79d4a4b81ee7a3480447cbeb1798e3ff01fea
pce0303 <ivychoen@gmail.com>
2023-08-16 01:54:02 +0900

    login

--- frontend/src/routes/login.js
+++ frontend/src/routes/login.js
@@ -3,12 +3,15 @@ import './login.css';
 
 function Login() {
     return (
-        <form method="post" action="/login" className="loginForm">
-            <h1 className="loginTitle">Log In</h1>
-            <input type="text" className="loginUsername" placeholder="Username"></input>
-            <input type="password" className="loginPassword" placeholder="Password"></input>
-            <button type="submit" className="loginButton">Login</button>
-        </form>
+        <div className="login-container">
+            <img className="background" alt="background" src="img/loginBackground.png" />
+            <form method="post" action="/login" className="loginForm">
+                <h1 className="loginTitle">Log In</h1>
+                <input type="text" className="loginUsername" placeholder="Username"></input>
+                <input type="password" className="loginPassword" placeholder="Password"></input>
+                <button type="submit" className="loginButton">Login</button>
+            </form>
+        </div>
     )
 }
 
