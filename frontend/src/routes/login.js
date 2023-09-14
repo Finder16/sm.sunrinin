e4176aaab11da3152eb2f21516817564e73ad9c4
pce0303 <ivychoen@gmail.com>
2023-09-15 00:50:52 +0900

    login

--- frontend/src/routes/login.js
+++ frontend/src/routes/login.js
@@ -3,15 +3,20 @@ import './login.css';
 
 function Login() {
     return (
+        <>
+        <video muted autoPlay loop>
+            <source src="img/background.mp4" type="video/mp4"/>
+        </video>
         <div className="login-container">
-            <img className="background" alt="background" src="img/loginBackground.png" />
+            <h1 className="loginTitle">Login</h1>
             <form method="post" action="/login" className="loginForm">
-                <h1 className="loginTitle">Log In</h1>
                 <input type="text" className="loginUsername" placeholder="Username"></input>
                 <input type="password" className="loginPassword" placeholder="Password"></input>
                 <button type="submit" className="loginButton">Login</button>
             </form>
         </div>
+        </>
+        
     )
 }
 
