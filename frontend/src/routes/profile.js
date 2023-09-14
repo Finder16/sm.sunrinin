e510fb3f88339532cbb0b92a24f0eb1a4569da90
pce0303 <ivychoen@gmail.com>
2023-09-15 00:51:10 +0900

    profile

--- frontend/src/routes/profile.js
+++ frontend/src/routes/profile.js
@@ -1,21 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
 import './profile.css';
-import profileImage from '../profileImage.png';
-import observationImage from '../observationImage.jpeg';
-
-// const public/img/observationImage.jpeg
+import profileImage from '../profileImage.webp';
+import observationImage from '../observationImage.webp';
+import BG from '../profileBG.gif';
+import camera from '../camera.webp';
 
 function Profile() {
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    const handleImageUpload = (e) => {
+        const file = e.target.files[0]; 
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                const dataURL = e.target.result;
+                setSelectedImage(dataURL);
+            };
+            reader.readAsDataURL(file);
+            // 파일 저장하는 코드 짤것?
+        };
+    }
+
     return (
+        <>
+        <img src={BG} alt="background" className="profileBG"/>
         <div className="profileBox">
-            <img src={profileImage} alt="profileImage" className="profileImage"></img>
+            <img src={selectedImage || profileImage} alt="profileImage" className="profileImage" />
+            <input
+                type="file"
+                accept="image/*"
+                onChange={handleImageUpload}
+                style={{ display: "none" }}
+                id="imageInput"
+            />
+            <label htmlFor="imageInput" className="uploadImg">
+                <img src={camera} alt="camera" />
+            </label>
             <p className="myName">이재영</p>
-            <p className="myInfo">자기소개 : 이재영 입니다.</p>
-            <p className="observationDate">관측 날짜 : 2023-06-14</p>
-            <img src={observationImage} alt="observationImage" className="observationImage"></img>   
-            <textarea className="commentContent"></textarea>
-            <button className="commentButton">코멘트 등록</button>
+            {/* <p className="myInfo">자기소개: 이재영입니다.</p>    */}
+            <p className="observationDate">관측 날짜: 2023-06-14</p>
+            <img src={observationImage} alt="observationImage" className="observationImage" />
+            <textarea className="commentContent" />
+            <button className="commentButton" placeholder="Write your comment here...">코멘트 등록</button>
         </div>
+        </>
+        
     )
 }
 
